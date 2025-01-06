34a824c7dec5a0428e066186bb0cea6cb3b34131
Kjetil Omang <kjetilom@gmail.com>
2025-01-06 14:15:27 +0100

    mininet multihop wireless config

--- mnw.py
+++ mnw.py
@@ -1,63 +1,97 @@
 #!/usr/bin/env python
 
-"""This example shows how to enable 4-address
-Warning: It works only when network manager is stopped"""
+'This example creates a simple network topology with 1 AP and 2 stations'
 
 import sys
 
-from mininet.node import Controller
 from mininet.log import setLogLevel, info
-from mn_wifi.link import wmediumd, _4address
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
+from mn_wifi.manetRoutingProtocols import batmand
+from mn_wifi.link import wmediumd, adhoc
 from mn_wifi.wmediumdConnector import interference
+from typing import Dict
+from mn_wifi.node import Station
+
+class WirelessNode:
+    def __init__(self, name):
+        pass
+
+class WirelessMesh:
+    def __init__(self, net: Mininet_wifi):
+        self.net = net
+        self.stations: Dict[str, Station] = {}
+
+    def set_default(self, **kwargs):
+        self.default = kwargs
+
+    def set_link_default(self, **kwargs):
+        self.link_default = kwargs
+
+    def add_station(self, name, cls=None, **kwargs):
+        return self.net.addStation(name, cls, **kwargs, **self.default)
+
+    def add_link(self, ):
+        pass
 
 
-def topology(args):
+
+
+def topology():
     "Create a network."
-    net = Mininet_wifi(controller=Controller, link=wmediumd,
-                       wmediumd_mode=interference, config4addr=True)
+    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
+
+    mesh = WirelessMesh(net=net)
+    mesh.set_default()
 
     info("*** Creating nodes\n")
-    ap1 = net.addAccessPoint('ap1', ssid="ap1-ssid", mode="g",
-                             channel="1", position='30,30,0')
-    ap2 = net.addAccessPoint('ap2', ssid="ap2-ssid", mode="g",
-                             channel="1", position='40,60,0')
-    ap3 = net.addAccessPoint('ap3', ssid="ap3-ssid", mode="g",
-                             channel="1", position='50,30,0')
-    sta1 = net.addStation('sta1', ip="192.168.0.1/24", position='31,32,0')
-    sta2 = net.addStation('sta2', ip="192.168.0.2/24", position='32,34,0')
-    sta3 = net.addStation('sta3', ip="192.168.0.3/24", position='41,62,0')
-    sta4 = net.addStation('sta4', ip="192.168.0.4/24", position='42,64,0')
-    sta5 = net.addStation('sta5', ip="192.168.0.5/24", position='51,32,0')
-    sta6 = net.addStation('sta6', ip="192.168.0.6/24", position='52,34,0')
-    c0 = net.addController('c0')
+    sta_args = {
+        "mode": "g",
+        "channel": 5,
+        "ssid": "adhocNet"
+    }
 
-    info("*** Configuring Propagation Model\n")
-    net.setPropagationModel(model="logDistance", exp=4.5)
+    # if '-v' in sys.argv:
+    #     sta_arg = {'nvif': 2}
+    # else:
+    #     # isolate_clientes: Client isolation can be used to prevent low-level
+    #     # bridging of frames between associated stations in the BSS.
+    #     # By default, this bridging is allowed.
+    #     # OpenFlow rules are required to allow communication among nodes
+    #     ap_arg = {'client_isolation': True}
+
+    sta1 = net.addStation('sta1', position="10,10,0", **sta_args, range=50, client_isolation=True)
+    sta2 = net.addStation("sta2", position="50,10,0", **sta_args, range=50, client_isolation=True)
+    sta3 = net.addStation("sta3", position="90,10,0", **sta_args, range=50, client_isolation=True)
+    # sta4 = net.addStation("sta4", position="90,50,0", **sta_args, range=50)
+    # sta5 = net.addStation("sta5", position="90,90,0", **sta_args, range=50)
+
+    net.setPropagationModel(model="logDistance", exp=4)
 
     info("*** Configuring nodes\n")
     net.configureNodes()
 
-    info("*** Adding Links\n")
-    net.addLink(ap1, ap2, cls=_4address)  # ap1=ap, ap2=client
-    net.addLink(ap1, ap3, cls=_4address)  # ap1=ap, ap3=client
-    net.addLink(sta1, ap1)
-    net.addLink(sta2, ap1)
-    net.addLink(sta3, ap2)
-    net.addLink(sta4, ap2)
-    net.addLink(sta5, ap3)
-    net.addLink(sta6, ap3)
+    info("*** Associating Stations\n")
+    net.addLink(sta1, cls=adhoc, intf='sta1-wlan0',
+            ssid='adhocNet', proto="batmand",
+            mode='g', channel=5)
+    net.addLink(sta2, cls=adhoc, intf='sta2-wlan0',
+            ssid='adhocNet', proto="batmand",
+            mode='g', channel=5)
+    net.addLink(sta3, cls=adhoc, intf='sta3-wlan0',
+            ssid='adhocNet', proto="batmand",
+            mode='g', channel=5)
+    # net.addLink(sta4, cls=adhoc, intf='sta4-wlan0',
+    #         ssid='adhocNet', proto="batmand",
+    #         mode='g', channel=5, ht_cap='HT40+')
+    # net.addLink(sta5, cls=adhoc, intf='sta5-wlan0',
+    #         ssid='adhocNet', proto="batmand",
+    #         mode='g', channel=5, ht_cap='HT40+')
 
-    if '-p' not in args:
-        net.plotGraph(max_x=100, max_y=100)
+    net.plotGraph(max_x=200, max_y=200)
 
     info("*** Starting network\n")
     net.build()
-    c0.start()
-    ap1.start([c0])
-    ap2.start([c0])
-    ap3.start([c0])
 
     info("*** Running CLI\n")
     CLI(net)
@@ -68,4 +102,4 @@ def topology(args):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    topology(sys.argv)
+    topology()
